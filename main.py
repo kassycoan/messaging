b7793e416272e302a71fea6e4879056f9aeda431
Kassy Coan <kassycoan@gmail.com>
2013-06-30 21:48:56 -0700

    Adds basic layout of inbox.

--- main.py
+++ main.py
@@ -14,11 +14,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from google.appengine.api import users
+
+import os
+import urllib
+
+import jinja2
 import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'])
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        user = users.get_current_user()
+
+        if not user:
+            self.redirect(users.create_login_url(self.request.uri))
+        else:
+            url = users.create_logout_url(self.request.uri)
+            template = JINJA_ENVIRONMENT.get_template('index.html')
+            template_values = {
+            'user': user,
+            'url_logout': url};
+            self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
